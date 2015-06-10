8818004c54768c94ab272a641cb20203ef545434
Javier <nano.world.contact@gmail.com>
2015-06-10 18:50:13 -0430

    arreglado el error de referencia en generadores

--- generators/routes.js
+++ generators/routes.js
@@ -1,8 +1,8 @@
 var path = require('path');
 
 module.exports = function(util, name) {
-  utils.read(path.join('frags', 'routeTemplate.js'))
+  util.read(path.join('frags', 'routeTemplate.js'))
     .then(function(out) {
-      utils.write(path.join('models', `${name}.js`), out, { name: name });
+      util.write(path.join('models', `${name}.js`), out, { name: name });
     });
 };
