b495e64462658a13b047c221d40311139575cb34
Javier <nano.world.contact@gmail.com>
2015-06-10 19:06:38 -0430

    little changes in generators

--- generators/routes.js
+++ generators/routes.js
@@ -1,8 +1,8 @@
 var path = require('path');
 
 module.exports = function(utils, name) {
-  utils.read(path.join('frags', 'routeTemplate.js'))
+  return utils.read(path.join('frags', 'routeTemplate.js'))
     .then(function(out) {
-      utils.write(path.join('models', `${name}.js`), out, { name: name });
+      return utils.write(path.join('models', `${name}.js`), out, { name: name });
     });
 };
