d17f3d4aaa1fcab63e633aae261c767617c44b40
Javier <nano.world.contact@gmail.com>
2015-06-15 18:59:27 -0430

    error de tipeo

--- generators/models.js
+++ generators/models.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = function(utils, name) {
-  return utils.src.read(path.join('templates', 'models.js'))
+  return utils.src.read(path.join('templates', 'model.js'))
     .then(function(out) {
       return utils.target.write(path.join('models', `${name}.js`), out, { name: name });
     })
--- generators/routes.js
+++ generators/routes.js
@@ -1,7 +1,7 @@
 var path = require('path');
 
 module.exports = function(utils, name) {
-  return utils.url.read(path.join('templates', 'routes.js'))
+  return utils.url.read(path.join('templates', 'route.js'))
     .then(function(out) {
       return utils.target.write(path.join('models', `${name}.js`), out, { name: name });
     });
